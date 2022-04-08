5a44e22391b16449777667d0ba9137047317d6fc
K <k@hyperelliptic.xyz>
2022-04-09 00:27:11 +0100

    Draft: begin outlining AvaLido and stAVAX contracts

--- src/AvaLido.sol
+++ src/AvaLido.sol
@@ -1,11 +1,86 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-FileCopyrightText: 2022 Hyperelliptic Labs and RockX
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.10;
 
-contract AvaLido {
+
+ █████╗ ██╗   ██╗ █████╗ ██╗     ██╗██████╗  ██████╗ 
+██╔══██╗██║   ██║██╔══██╗██║     ██║██╔══██╗██╔═══██╗
+███████║██║   ██║███████║██║     ██║██║  ██║██║   ██║
+██╔══██║╚██╗ ██╔╝██╔══██║██║     ██║██║  ██║██║   ██║
+██║  ██║ ╚████╔╝ ██║  ██║███████╗██║██████╔╝╚██████╔╝
+╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚═╝╚═════╝  ╚═════╝      
+     
+                         ,██▄
+                        /█████
+                       ████████
+                      ████████
+                    ,████████   ,,
+                   ▄████████   ████
+                  ████████    ██████
+                 ████████    ████████
+     
+              ████                 ,███
+             ████████▌         ,████████
+             ████████████,  █████████████
+            ]████████████████████████████
+             ████████████████████████████
+             ███████████████████████████▌
+              ██████████████████████████
+               ███████████████████████
+                 ███████████████████
+                    ╙████████████
+
+
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuard.sol";
+
+contract AvaLido is PausableUpgradeable, ReentrancyGuardUpgradeable {
     // main contract that is single source of truth for state variables,
     // coordinates other contracts, accepts deposits and withdrawal requests, etc.
 
-    function deposit() public {}
+    // Events
 
-    function requestWithdrawal() public {}
+    event DepositEvent(address indexed _from, uint256 indexed _amount);
+    event WithdrawRequestSubmittedEvent(address indexed _from, uint256 indexed _amount, uint256 timestamp);
+    // WithdrawRequestFilled
+    // WithdrawalRequestClaimed
+    // WithdrawalRequestCompleted
+    
+    // States variables
+
+    /**
+     * @dev Receives AVAX and mints StAVAX to msg.sender
+     * @param amount - Amount of AVAX sent from msg.sender
+     * @return Amount of StAVAX shares generated
+     */
+    function deposit(uint256 amount)
+        external
+        whenNotPaused
+        nonReentrant
+        returns (uint256)
+    {
+        require(_amount > 0, "Invalid amount");
+
+        // 1. convertAvaxToStAvax
+        // 2. mint stAVAX to user
+        // 3. do some calculations and either use the deposit money to fill withdrawal requests or be staked
+
+        emit SubmitEvent(msg.sender, amount);
+        // 4. returns amount of stAVAX shares generated
+    }
+
+    /**
+     * @dev Requests to unstake an amount of AVAX.
+     */
+    function requestWithdrawal(uint256 amount, uint256 timestamp)
+        external
+        whenNotPaused
+        nonReentrant
+    {
+        // 1. convertStAvaxToAvax
+        // 2. burn stAVAX
+        // 3. add request to withdrawal queue
+        
+        emit WithdrawRequestSubmittedEvent(msg.sender, amount, timestamp);
+    }
 }
--- src/stAVAX.sol
+++ src/stAVAX.sol
@@ -1,10 +1,80 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-FileCopyrightText: 2022 Hyperelliptic Labs and RockX
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 contract stAVAX {
     // the ERC-20 implementation of a rebasing token 1:1 pegged to AVAX
+    function mint() public {
 
-    function mint() public {}
+    }
 
-    function burn() public {}
+    function burn() public {
+
+    }
+
+    // Note: does this stuff need to be in the main contract as helpers?
+
+    /**
+     * @dev Function that calculates total pooled Avax
+     * @return Total pooled Avax
+     */
+    function gettotalPooledAvax() public view returns (uint256) {
+        uint256 totalStaked = // need to figure this out from oracle contract
+        return totalStaked + totalBuffered - reservedFunds;
+    }
+
+    /**
+     * @dev Function that converts arbitrary Avax to stAVAX
+     * @param _balance - Balance in Avax
+     * @return Balance in stAVAX, totalShares and totalPooledAvax
+     */
+    function convertAvaxToStAvax(uint256 _balance)
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        uint256 totalShares = totalSupply();
+        // If total supply is 0 (i.e. first person depositing) then totalShares is 1 bc they have it all
+        totalShares = totalShares == 0 ? 1 : totalShares;
+
+        uint256 totalPooledAvax = gettotalPooledAvax();
+        totalPooledAvax = totalPooledAvax == 0 ? 1 : totalPooledAvax;
+
+        uint256 balanceInStAvax = (_balance * totalShares) / totalPooledAvax;
+
+        return (balanceInStAvax, totalShares, totalPooledAvax);
+    }
+
+    /**
+     * @dev Function that converts arbitrary stAVAX to Avax
+     * @param _balance - Balance in stAVAX
+     * @return Balance in Avax, totalShares and totalPooledAvax
+     */
+    function convertStAvaxToAvax(uint256 _balance)
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        uint256 totalShares = totalSupply();
+        totalShares = totalShares == 0 ? 1 : totalShares;
+
+        uint256 totalPooledAvax = gettotalPooledAvax();
+        totalPooledAvax = totalPooledAvax == 0 ? 1 : totalPooledAvax;
+
+        uint256 balanceInAvax = (_balance * totalPooledAvax) / totalShares;
+
+        return (balanceInAvax, totalShares, totalPooledAvax);
+    }
 }
