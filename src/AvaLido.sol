95d2f6d37a0a2e777c251695278d56f89ab5bc58
Will Demaine <willdemaine@gmail.com>
2022-04-12 18:29:45 +0700

    Cleanup, comments, add errors, tests (#6)
    * Cleanup, comments, add errors, tests
    
    * Use smaller const, smaller time, reorder struct for packing
    
    * Fix tests

--- src/AvaLido.sol
+++ src/AvaLido.sol
@@ -38,20 +38,29 @@ import "openzeppelin-contracts/contracts/utils/math/Math.sol";
 import "./test/console.sol";
 
 struct UnstakeRequest {
-    address requester;
-    uint256 amountRequested;
-    uint256 amountFilled;
-    uint256 requestedAt;
+    address requester; // The user who requested the unstake.
+    uint64 requestedAt; // The block.timestamp when the unstake request was made.
+    uint256 amountRequested; // The amount of stAVAX requested to be unstaked.
+    uint256 amountFilled; // The amount of free'd AVAX that has been allocated to this request.
 }
 
+uint64 constant MINIMUM_STAKE_AMOUNT = 0.1 ether;
+uint8 constant MAXIMUM_UNSTAKE_REQUESTS = 10;
+
+/**
+ * @title Lido on Avalanche
+ * @author Hyperelliptic Labs and RockX
+ */
 contract AvaLido is Pausable, ReentrancyGuard {
-    // main contract that is single source of truth for state variables,
-    // coordinates other contracts, accepts deposits and withdrawal requests, etc.
-
     // Events
-
-    event DepositEvent(address indexed _from, uint256 indexed _amount);
+    event DepositEvent(address indexed _from, uint256 indexed _amount, uint256 timestamp);
     event WithdrawRequestSubmittedEvent(address indexed _from, uint256 indexed _amount, uint256 timestamp);
+
+    // Errors
+    error InvalidStakeAmount();
+    error TooManyConcurrentUnstakeRequests();
+    error NotAuthorized();
+    error ClaimTooLarge();
 
     // Emitted to signal the MPC system to stake AVAX.
     // TODO: Move to mpc manager contract
@@ -74,63 +83,129 @@ contract AvaLido is Pausable, ReentrancyGuard {
     // Pointer to the head of the unfilled section of the queue.
     uint256 private unfilledHead = 0;
 
-    /**
-     * @dev Receives AVAX and mints StAVAX to msg.sender
-     * @param amount - Amount of AVAX sent from msg.sender
-     */
-    function deposit(uint256 amount) external payable whenNotPaused nonReentrant {
-        require(amount > 0, "Invalid amount");
-        require(amount == msg.value, "Not enough AVAX sent");
+    // Record the number of unstake requests per user so that we can limit them to our max.
+    mapping(address => uint8) public unstakeRequestCount;
 
-        // STAVAX.mint();
-        // Send stAVAX to user
-
-        emit DepositEvent(msg.sender, amount);
-        uint256 remaining = fillUnstakeRequests(amount);
-        _stake(remaining);
-    }
+    // -------------------------------------------------------------------------
+    //  Public functions
+    // -------------------------------------------------------------------------
 
     /**
-     * @dev Requests to unstake an amount of AVAX.
+     * @notice Return your stAVAX to receive the equivalent amount of AVAX.
+     * @dev We limit users to some maximum number of concurrent unstake requests to prevent
+     * people flooding the queue. The amount for each unstake request is unbounded.
+     * @param amount The amount of stAVAX to unstake.
      */
     function requestWithdrawal(uint256 amount) external whenNotPaused nonReentrant returns (uint256) {
-        // TODO: Transfer stAVAX from user to our contract.
+        if (amount == 0) revert InvalidStakeAmount();
 
-        // Create the request and store in our list.
-        unstakeRequests.push(UnstakeRequest(msg.sender, amount, 0, block.timestamp));
+        // TODO: Transfer stAVAX from user to our contract.
+        if (unstakeRequestCount[msg.sender] == MAXIMUM_UNSTAKE_REQUESTS) {
+            revert TooManyConcurrentUnstakeRequests();
+        }
+        unstakeRequestCount[msg.sender]++;
+
+        // Create the request and store in our queue.
+        unstakeRequests.push(UnstakeRequest(msg.sender, uint64(block.timestamp), amount, 0));
 
         emit WithdrawRequestSubmittedEvent(msg.sender, amount, block.timestamp);
 
         return unstakeRequests.length - 1;
     }
 
-    function requestByIndex(uint32 requestIndex) external view returns (UnstakeRequest memory) {
+    /**
+     * @notice Look up an unstake request by index in the queue.
+     * @dev As the queue is append-only, we can simply return the request at the given index.
+     * @param requestIndex index The index of the request to look up.
+     * @return UnstakeRequest The request at the given index.
+     */
+    function requestByIndex(uint256 requestIndex) external view returns (UnstakeRequest memory) {
         return unstakeRequests[requestIndex];
     }
 
-    function claim(uint32 requestIndex, uint256 amount) external whenNotPaused nonReentrant {
+    /**
+     * @notice Claim your AVAX from a completed unstake requested.
+     * @dev This allows users to claim their AVAX back. We burn the stAVAX that we've been holding
+     * at this point, so that the amount of AVAX in the protocol aligns with the amount of stAVAX
+     * in circulation, and rebases can be computed properly.
+     * Note that we also allow partial claims of unstake requests so that users don't need to wait
+     * for the entire request to be filled to get some liquidity.
+     */
+    function claim(uint256 requestIndex, uint256 amount) external whenNotPaused nonReentrant {
         // TODO: Find request by ID
         UnstakeRequest memory request = this.requestByIndex(requestIndex);
 
-        require(request.requester == msg.sender, "Can only claim your own requests");
-        require(amount <= request.amountFilled, "Can only claim what is filled");
+        if (request.requester != msg.sender) revert NotAuthorized();
+        if (amount > request.amountFilled) revert ClaimTooLarge();
 
         // Partial claim, update amounts.
         // TODO: Should we maintain 'amountClaimed' instead of changing these fields?
-        request.amountRequested -= amount;
-        request.amountFilled -= amount;
+        // request.amountRequested -= amount;
+        // request.amountFilled -= amount;
 
         // Burn {amount} stAVAX owned by the contract
         // Transfer {amount} stAVAX to the user
 
         // Emit claim event.
-        if (request.amountFilled + amount == request.amountRequested) {
+        if (amount == request.amountRequested) {
             // TODO.
             // Final claim, remove this request.
+            unstakeRequestCount[msg.sender]--;
             return;
         }
     }
 
+    // -------------------------------------------------------------------------
+    //  Payable functions
+    // -------------------------------------------------------------------------
+
+    /**
+     * @notice Depsoit your AXAV to receive Staked AVAX (stAVAX) in return.
+     * You will always receive stAVAX in a 1:1 ratio.
+     * @dev Receives AVAX and mints StAVAX to msg.sender. We attempt to fill
+     * any outstanding requests with the incoming AVAX for instant liquidity.
+     */
+    function deposit() external payable whenNotPaused nonReentrant {
+        uint256 amount = msg.value;
+        if (amount < MINIMUM_STAKE_AMOUNT) revert InvalidStakeAmount();
+
+        // STAVAX.mint();
+        // Send stAVAX to user
+
+        emit DepositEvent(msg.sender, amount, block.timestamp);
+        uint256 remaining = fillUnstakeRequests(amount);
+        _stake(remaining);
+    }
+
+    /**
+     * @notice You should not call this funciton.
+     * @dev A payable function which receives AVAX from the MPC wallet and
+     * uses it to fill unstake requests. Any remaining funds after all requests
+     * are filled are re-staked.
+     */
+    function receiveFromMPC() external payable {
+        // Fill unstake requests
+        uint256 remaining = fillUnstakeRequests(msg.value);
+
+        // Rebalance liquidity pool
+
+        // Restake excess
+        _stake(remaining);
+    }
+
+    // -------------------------------------------------------------------------
+    //  Private/internal functions
+    // -------------------------------------------------------------------------
+
+    /**
+     * @dev Fills the next available unstake request with the given amount.
+     * This function works by reading the `unstakeRequests` queue, in-order, starting
+     * from the `unfilledHead` pointer. When a request is completely filled, we update
+     * the `unfilledHead` pointer to the next request.
+     * Note that filled requests are not removed from the queue, as they still must be
+     * claimed by users.
+     * @param inputAmount The amount of free'd AVAX made available to fill requests.
+     */
     function fillUnstakeRequests(uint256 inputAmount) private returns (uint256) {
         // Fill as many unstake requests as possible
         uint256 amountFilled = 0;
@@ -139,6 +214,7 @@ contract AvaLido is Pausable, ReentrancyGuard {
         // Assumes order of the array is creation order.
         for (uint256 i = unfilledHead; i < unstakeRequests.length; i++) {
             if (amountFilled == inputAmount) {
+                // This shouldn't happen, but revert if it does for clearer testing
                 revert("Invalid state - filled request in queue");
             }
 
@@ -163,15 +239,6 @@ contract AvaLido is Pausable, ReentrancyGuard {
 
     function isFilled(UnstakeRequest memory request) private pure returns (bool) {
         return request.amountFilled == request.amountRequested;
-    }
-
-    function receiveFromMPC() external payable {
-        // Fill unstake requests
-        uint256 remaining = fillUnstakeRequests(msg.value);
-        // Rebalance liquidity pool
-
-        // Restake excess
-        _stake(remaining);
     }
 
     function _stake(uint256 amount) internal {
--- src/test/AvaLido.t.sol
+++ src/test/AvaLido.t.sol
@@ -20,37 +20,50 @@ contract AvaLidoTest is DSTest {
     // Deposit
 
     function testStakeBasic() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
     }
 
-    function testFailStakeNotEnoughAVAX() public {
-        lido.deposit{value: 0.9 ether}(1 ether);
-    }
-
-    function testFailStakeZeroDeposit() public {
-        lido.deposit{value: 0 ether}(0 ether);
+    function testStakeZeroDeposit() public {
+        cheats.expectRevert(AvaLido.InvalidStakeAmount.selector);
+        lido.deposit{value: 0 ether}();
     }
 
     // Unstake Requests
 
+    function testUnstakeRequestZeroAmount() public {
+        cheats.expectRevert(AvaLido.InvalidStakeAmount.selector);
+        lido.requestWithdrawal(0 ether);
+    }
+
+    function testTooManyConcurrentUnstakes() public {
+        lido.deposit{value: 100 ether}();
+        // Do all the allowed requests
+        for (uint256 i = 1; i <= MAXIMUM_UNSTAKE_REQUESTS; i++) {
+            lido.requestWithdrawal(1 ether);
+        }
+        // Try one more
+        cheats.expectRevert(AvaLido.TooManyConcurrentUnstakeRequests.selector);
+        lido.requestWithdrawal(1 ether);
+    }
+
     function testUnstakeRequest() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
         // TODO: Test stAVAX transfer.
 
         uint256 requestId = lido.requestWithdrawal(0.5 ether);
         assertEq(requestId, 0);
 
-        (address requester, uint256 amountRequested, uint256 amountFilled, uint256 requestAt) = lido.unstakeRequests(
+        (address requester, uint64 requestAt, uint256 amountRequested, uint256 amountFilled) = lido.unstakeRequests(
             requestId
         );
 
         assertEq(requester, TEST_ADDRESS);
         assertEq(amountRequested, 0.5 ether);
         assertEq(amountFilled, 0 ether);
-        assertEq(requestAt, block.timestamp);
+        assertEq(requestAt, uint64(block.timestamp));
 
         uint256 requestId2 = lido.requestWithdrawal(0.1 ether);
-        (address requester2, uint256 amountRequested2, uint256 amountFilled2, uint256 requestAt2) = lido
+        (address requester2, uint256 requestAt2, uint256 amountRequested2, uint256 amountFilled2) = lido
             .unstakeRequests(requestId2);
 
         assertEq(requestId2, 1);
@@ -58,65 +71,65 @@ contract AvaLidoTest is DSTest {
         assertEq(requester2, TEST_ADDRESS);
         assertEq(amountRequested2, 0.1 ether);
         assertEq(amountFilled2, 0 ether);
-        assertEq(requestAt2, block.timestamp);
+        assertEq(requestAt2, uint64(block.timestamp));
     }
 
     function testFillUnstakeRequestSingle() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
         lido.requestWithdrawal(0.5 ether);
         lido.receiveFromMPC{value: 0.5 ether}();
 
-        (, uint256 amountRequested, uint256 amountFilled, ) = lido.unstakeRequests(0);
+        (, , uint256 amountRequested, uint256 amountFilled) = lido.unstakeRequests(0);
 
         assertEq(amountRequested, 0.5 ether);
         assertEq(amountFilled, 0.5 ether);
     }
 
     function testMultipleFillUnstakeRequestsSingleFill() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
         lido.requestWithdrawal(0.5 ether);
         lido.requestWithdrawal(0.25 ether);
         lido.requestWithdrawal(0.1 ether);
         lido.receiveFromMPC{value: 2 ether}();
 
-        (, uint256 amountRequested, uint256 amountFilled, ) = lido.unstakeRequests(0);
+        (, , uint256 amountRequested, uint256 amountFilled) = lido.unstakeRequests(0);
         assertEq(amountRequested, 0.5 ether);
         assertEq(amountFilled, 0.5 ether);
 
-        (, uint256 amountRequested2, uint256 amountFilled2, ) = lido.unstakeRequests(1);
+        (, , uint256 amountRequested2, uint256 amountFilled2) = lido.unstakeRequests(1);
         assertEq(amountRequested2, 0.25 ether);
         assertEq(amountFilled2, 0.25 ether);
 
-        (, uint256 amountRequested3, uint256 amountFilled3, ) = lido.unstakeRequests(2);
+        (, , uint256 amountRequested3, uint256 amountFilled3) = lido.unstakeRequests(2);
         assertEq(amountRequested3, 0.1 ether);
         assertEq(amountFilled3, 0.1 ether);
     }
 
     function testFillUnstakeRequestPartial() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
         uint256 reqId = lido.requestWithdrawal(0.5 ether);
         lido.receiveFromMPC{value: 0.1 ether}();
 
-        (, uint256 amountRequested, uint256 amountFilled, ) = lido.unstakeRequests(reqId);
+        (, , uint256 amountRequested, uint256 amountFilled) = lido.unstakeRequests(reqId);
 
         assertEq(amountRequested, 0.5 ether);
         assertEq(amountFilled, 0.1 ether);
     }
 
     function testFillUnstakeRequestPartialMultiple() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
         lido.requestWithdrawal(0.5 ether);
         lido.receiveFromMPC{value: 0.1 ether}();
         lido.receiveFromMPC{value: 0.1 ether}();
 
-        (, uint256 amountRequested, uint256 amountFilled, ) = lido.unstakeRequests(0);
+        (, , uint256 amountRequested, uint256 amountFilled) = lido.unstakeRequests(0);
 
         assertEq(amountRequested, 0.5 ether);
         assertEq(amountFilled, 0.2 ether);
     }
 
     function testFillUnstakeRequestPartialMultipleFilled() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
         lido.requestWithdrawal(0.5 ether);
         lido.receiveFromMPC{value: 0.1 ether}();
 
@@ -126,14 +139,14 @@ contract AvaLidoTest is DSTest {
 
         lido.receiveFromMPC{value: 1 ether}();
 
-        (, uint256 amountRequested, uint256 amountFilled, ) = lido.unstakeRequests(0);
+        (, , uint256 amountRequested, uint256 amountFilled) = lido.unstakeRequests(0);
 
         assertEq(amountRequested, 0.5 ether);
         assertEq(amountFilled, 0.5 ether);
     }
 
     function testMultipleRequestReads() public {
-        lido.deposit{value: 1 ether}(1 ether);
+        lido.deposit{value: 1 ether}();
         uint256 reqId = lido.requestWithdrawal(0.5 ether);
 
         // Make a request as somebody else
@@ -146,5 +159,37 @@ contract AvaLidoTest is DSTest {
         assertEq(reqId, 0);
         // Ensure that the next id for the user is the 3rd overall, not second.
         assertEq(reqId2, 2);
+    }
+
+    // Claiming
+
+    function testClaimOwnedByOtherUser() public {
+        lido.deposit{value: 1 ether}();
+        uint256 reqId = lido.requestWithdrawal(0.5 ether);
+        lido.receiveFromMPC{value: 0.5 ether}();
+
+        // Make a request as somebody else
+        cheats.prank(ZERO_ADDRESS);
+        cheats.expectRevert(AvaLido.NotAuthorized.selector);
+        lido.claim(reqId, 0.5 ether);
+    }
+
+    function testClaimTooLarge() public {
+        lido.deposit{value: 1 ether}();
+        uint256 reqId = lido.requestWithdrawal(0.5 ether);
+        lido.receiveFromMPC{value: 0.5 ether}();
+
+        cheats.expectRevert(AvaLido.ClaimTooLarge.selector);
+        lido.claim(reqId, 1 ether);
+    }
+
+    function testClaimSucceeds() public {
+        lido.deposit{value: 1 ether}();
+        uint256 reqId = lido.requestWithdrawal(0.5 ether);
+        lido.receiveFromMPC{value: 0.5 ether}();
+
+        assertEq(lido.unstakeRequestCount(TEST_ADDRESS), 1);
+        lido.claim(reqId, 0.5 ether);
+        assertEq(lido.unstakeRequestCount(TEST_ADDRESS), 0);
     }
 }
