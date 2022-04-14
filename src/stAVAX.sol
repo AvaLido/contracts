3b57307ca52fddada5567979570895183c83c104
Will Demaine <willdemaine@gmail.com>
2022-04-14 10:20:19 +0700

    Documentation, renaming, tests

--- src/stAVAX.sol
+++ src/stAVAX.sol
@@ -4,6 +4,13 @@ pragma solidity 0.8.10;
 
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @notice stAVAX tokens are liquid staked AVAX tokens.
+ * @dev ERC-20 implementation of a rebasing token 1:1 pegged to AVAX.
+ * This contract is abstract, and must be implemented by something which
+ * knows the total amount of controlled AVAX.
+ * TODO: Transfers, approvals, events.
+ */
 abstract contract stAVAX is ERC20 {
     uint256 private totalShares = 0;
 
@@ -14,15 +21,24 @@ abstract contract stAVAX is ERC20 {
 
     mapping(address => uint256) private shares;
 
+    /**
+     * @notice The total supply of stAVAX tokens
+     * @dev Because we are 1:1 with AVAX, this is simply the amount of
+     * AVAX that the protocol controls.
+     */
     function totalSupply() public view override returns (uint256) {
-        return getTotalPooledAvax();
+        return getProtocolControlledAVAX();
     }
 
     function balanceOf(address account) public view override returns (uint256) {
         return getBalanceByShares(shares[account]);
     }
 
-    // the ERC-20 implementation of a rebasing token 1:1 pegged to AVAX
+    /**
+     * @dev Mint tokens to a given address.
+     * This is simply the act of increasing the total supply and allocating shares
+     * to the given address.
+     */
     function mint(address recipient, uint256 amount) internal {
         if (recipient == address(0)) revert CannotMintToZeroAddress();
 
@@ -30,6 +46,9 @@ abstract contract stAVAX is ERC20 {
         shares[recipient] += amount;
     }
 
+    /**
+     * @dev Burn tokens from a given address.
+     */
     function burn(address owner, uint256 amount) internal {
         if (shares[owner] < amount) revert NotEnoughBalance();
 
@@ -38,15 +57,22 @@ abstract contract stAVAX is ERC20 {
     }
 
     /**
-     * @dev Function that calculates total pooled Avax
-     * @return Total pooled Avax
+     * @dev The total protocol controlled AVAX. Must be implemented by the
+     * owning contract.
+     * @return amount protocol controlled AVAX
      */
-    function getTotalPooledAvax() public view virtual returns (uint256);
+    function getProtocolControlledAVAX() public view virtual returns (uint256);
 
-    function getBalanceByShares(uint256 _sharesAmount) public view returns (uint256) {
-        if (totalShares == 0) {
+    /**
+     * @dev Computes the total amount of AVAX represented by a number of shares.
+     * Note that this can slightly underreport due to rounding down of the division.
+     * @param sharesAmount number of shares
+     * @return amount of AVAX represented by shares
+     */
+    function getBalanceByShares(uint256 sharesAmount) private view returns (uint256) {
+        if (totalShares == 0 || sharesAmount == 0) {
             return 0;
         }
-        return (_sharesAmount * getTotalPooledAvax()) / totalShares;
+        return (sharesAmount * getProtocolControlledAVAX()) / totalShares;
     }
 }
--- src/test/stAVAX.t.sol
+++ src/test/stAVAX.t.sol
@@ -1,18 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
+// import "ds-test/src/test.sol";
 import "ds-test/test.sol";
+import "./cheats.sol";
+import "./console.sol";
 import "../stAVAX.sol";
 
-contract TestToken is stAVAX {
-    uint256 public totalPooled = 0;
+address constant USER1_ADDRESS = 0x0000000000000000000000000000000000000001;
+address constant USER2_ADDRESS = 0x0000000000000000000000000000000000000002;
 
-    function _setTotalPooled(uint256 _totalPooled) public {
-        totalPooled = _totalPooled;
+contract TestToken is stAVAX {
+    uint256 public totalControlled = 0;
+
+    function _setTotalControlled(uint256 _totalControlled) public {
+        totalControlled = _totalControlled;
     }
 
-    function getTotalPooledAvax() public view override returns (uint256) {
-        return totalPooled;
+    function getProtocolControlledAVAX() public view override returns (uint256) {
+        return totalControlled;
+    }
+
+    function proxyMint(address recipient, uint256 amount) public {
+        super.mint(recipient, amount);
+    }
+
+    function proxyBurn(address owner, uint256 amount) public {
+        super.burn(owner, amount);
     }
 }
 
@@ -23,7 +37,58 @@ contract stAVAXTest is DSTest {
         stavax = new TestToken();
     }
 
-    function testExample() public {
-        assertTrue(true);
+    function testSharesSingleUser() public {
+        stavax.proxyMint(USER1_ADDRESS, 100);
+        stavax._setTotalControlled(100);
+
+        assertEq(stavax.totalSupply(), 100);
+        assertEq(stavax.balanceOf(USER1_ADDRESS), 100);
+    }
+
+    function testSharesSingleUserNotEqual() public {
+        stavax.proxyMint(USER1_ADDRESS, 100);
+        stavax._setTotalControlled(50);
+
+        assertEq(stavax.balanceOf(USER1_ADDRESS), 50);
+    }
+
+    function testSharesMultipleUser() public {
+        stavax.proxyMint(USER1_ADDRESS, 100);
+        stavax.proxyMint(USER2_ADDRESS, 100);
+        stavax._setTotalControlled(100);
+
+        assertEq(stavax.balanceOf(USER1_ADDRESS), 50);
+        assertEq(stavax.balanceOf(USER2_ADDRESS), 50);
+    }
+
+    function testSharesMultipleUserNotEqual() public {
+        stavax.proxyMint(USER1_ADDRESS, 2);
+        stavax.proxyMint(USER2_ADDRESS, 8);
+        stavax._setTotalControlled(50);
+
+        assertEq(stavax.balanceOf(USER1_ADDRESS), 10);
+        assertEq(stavax.balanceOf(USER2_ADDRESS), 40);
+    }
+
+    function testSharesMultipleUserWithFuzzing(uint256 u1Amount, uint256 u2Amount) public {
+        // AVAX total supply ~300m
+        cheats.assume(u1Amount < 300_000_000 ether);
+        cheats.assume(u2Amount < 300_000_000 ether);
+
+        stavax.proxyMint(USER1_ADDRESS, u1Amount);
+        stavax.proxyMint(USER2_ADDRESS, u2Amount);
+        stavax._setTotalControlled(u1Amount + u2Amount);
+
+        assertEq(stavax.balanceOf(USER1_ADDRESS), u1Amount);
+        assertEq(stavax.balanceOf(USER2_ADDRESS), u2Amount);
+    }
+
+    function testSharesMultipleUserNotRound() public {
+        stavax.proxyMint(USER1_ADDRESS, 2 ether);
+        stavax.proxyMint(USER2_ADDRESS, 1 ether);
+        stavax._setTotalControlled(100 ether);
+
+        assertEq(stavax.balanceOf(USER1_ADDRESS), 66666666666666666666);
+        assertEq(stavax.balanceOf(USER2_ADDRESS), 33333333333333333333);
     }
 }
