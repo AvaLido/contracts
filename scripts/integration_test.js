ece6dd7efe36613893b1de0229c45394ee3be656
James Nicholson <git@nicholson.io>
2022-04-20 19:37:42 +0100

    Take address on command line args

--- scripts/integration_test.js
+++ scripts/integration_test.js
@@ -2,6 +2,15 @@ const { ethers } = require("ethers");
 const fs = require("fs");
 const utils = ethers.utils;
 
+// Command line args
+const args = process.argv;
+if (args.length < 3) {
+  const filename = require("path").basename(__filename);
+  console.log("Usage: node " + filename + " [contract address]");
+  process.exit();
+}
+const address = args[2];
+
 // RPC
 const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:9650/ext/bc/C/rpc");
 const signer = provider.getSigner();
@@ -12,7 +21,6 @@ const wendy = new ethers.Wallet("a7f3a9981d794d4849f296b0406bd4ee9aa5bfa03208954
 const sammy = new ethers.Wallet("0d4c5da04cb1a1292ac933f49722a49f20e4284ab268d4cd31119ac90e91117e", provider);
 
 // Contract
-const address = "0x52c84043cd9c865236f11d9fc9f56aa003c1f922";
 const avalido = JSON.parse(fs.readFileSync("out/AvaLido.sol/AvaLido.json"));
 const abi = avalido["abi"];
 const contract = new ethers.Contract(address, abi, deployer);
