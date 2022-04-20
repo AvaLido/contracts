146948162670b1f3fb74d0214a55daf7ccb22a8a
Will Demaine <willdemaine@gmail.com>
2022-04-20 18:57:46 +0200

    Add readme instructions and helper script

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+VAL=$(task deploy -- ValidatorOracle | grep -i "deployed" | cut -d " " -f 3)
+MANAGER=$(task deploy -- ValidatorManager --constructor-args $VAL | grep -i "deployed" | cut -d " " -f 3)
+task deploy -- AvaLido --constructor-args "0x2000000000000000000000000000000000000001" "0x2000000000000000000000000000000000000002" $MANAGER "0x3000000000000000000000000000000000000001"
