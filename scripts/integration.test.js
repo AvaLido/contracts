95597d895e67a34693db5d1958e04aa6fc07e9da
James Nicholson <git@nicholson.io>
2022-04-24 10:49:24 +0100

    Basic contract error code decoding

--- scripts/integration.test.js
+++ scripts/integration.test.js
@@ -30,6 +30,28 @@ async function setUpValidator() {
   let result = await validator_contract.selectValidatorsForStake(utils.parseEther("1000").toString());
 }
 
+function decodeAndRethrowError(error) {
+  const codes = [
+    "InvalidStakeAmount()",
+    "TooManyConcurrentUnstakeRequests()",
+    "NotAuthorized()",
+    "ClaimTooLarge()",
+    "InsufficientBalance()",
+    "NoAvailableValidators()",
+  ];
+
+  // TODO: This is bad and you should feel bad
+  const error_code = error.message.split('"data":"')[1].slice(0, 10);
+
+  for (const code of codes) {
+    const hash = utils.keccak256(utils.toUtf8Bytes(code));
+    if (hash.startsWith(error_code)) {
+      throw "Error from contract: " + code;
+    }
+  }
+  throw "Unrecognized error from contract. Check that all errors have been imported.";
+}
+
 beforeAll(() => {
   return setUpValidator();
 });
@@ -47,50 +69,58 @@ test("Make a deposit", async () => {
 });
 
 test("Request a withdrawal", async () => {
-  const start_balance = await contract.balanceOf(deployer.address);
-  const withdrawal_amount = utils.parseEther("10");
+  try {
+    const start_balance = await contract.balanceOf(deployer.address);
+    const withdrawal_amount = utils.parseEther("10");
 
-  const withdrawal = await contract.requestWithdrawal(withdrawal_amount);
-  await withdrawal.wait();
+    const withdrawal = await contract.requestWithdrawal(withdrawal_amount);
+    await withdrawal.wait();
 
-  const end_balance = await contract.balanceOf(deployer.address);
-  expect(end_balance.toString()).toBe(start_balance.sub(withdrawal_amount).toString());
+    const end_balance = await contract.balanceOf(deployer.address);
+    expect(end_balance.toString()).toBe(start_balance.sub(withdrawal_amount).toString());
+  } catch (error) {
+    decodeAndRethrowError(error);
+  }
 });
 
 test("Randomly deposit or withdraw 100 times", async () => {
-  const start_balance = await contract.balanceOf(deployer.address);
+  try {
+    const start_balance = await contract.balanceOf(deployer.address);
 
-  let deposits_cumulative = utils.parseEther("0");
-  let withdrawals_cumulative = utils.parseEther("0");
+    let deposits_cumulative = utils.parseEther("0");
+    let withdrawals_cumulative = utils.parseEther("0");
 
-  for (i = 0; i < 100; i++) {
-    const balance = await contract.balanceOf(deployer.address);
+    for (i = 0; i < 100; i++) {
+      const balance = await contract.balanceOf(deployer.address);
 
-    let raw_amount = Math.pow(2 * Math.random() + 0.5, 10); // Roughly 0.001 to 10k eth
-    raw_amount = Math.round(raw_amount * 1000) / 1000;
-    const amount = utils.parseEther(raw_amount.toString());
+      let raw_amount = Math.pow(2 * Math.random() + 0.5, 10); // Roughly 0.001 to 10k eth
+      raw_amount = Math.round(raw_amount * 1000) / 1000;
+      const amount = utils.parseEther(raw_amount.toString());
 
-    // Prevent attempting to withdraw more than is deposited
-    const isWithdrawal = Math.random() < 0.5 && balance.gt(amount);
+      // Prevent attempting to withdraw more than is deposited
+      const isWithdrawal = Math.random() < 0.5 && balance.gt(amount);
 
-    if (isWithdrawal) {
-      withdrawals_cumulative = withdrawals_cumulative.add(amount);
-      console.log(
-        "Withdrawal " + utils.formatEther(amount) + " (Cumulative " + utils.formatEther(withdrawals_cumulative) + ")"
-      );
-      const withdrawal = await contract.requestWithdrawal(amount);
-      await withdrawal.wait(); // TODO: Should wait at end rather than every step
-    } else {
-      deposits_cumulative = deposits_cumulative.add(amount);
-      console.log(
-        "Deposit " + utils.formatEther(amount) + " (Cumulative " + utils.formatEther(withdrawals_cumulative) + ")"
-      );
-      const deposit = await contract.deposit({ value: amount });
-      await deposit.wait();
+      if (isWithdrawal) {
+        withdrawals_cumulative = withdrawals_cumulative.add(amount);
+        console.log(
+          "Withdrawal " + utils.formatEther(amount) + " (Cumulative " + utils.formatEther(withdrawals_cumulative) + ")"
+        );
+        const withdrawal = await contract.requestWithdrawal(amount);
+        await withdrawal.wait(); // TODO: Should wait at end rather than every step
+      } else {
+        deposits_cumulative = deposits_cumulative.add(amount);
+        console.log(
+          "Deposit " + utils.formatEther(amount) + " (Cumulative " + utils.formatEther(withdrawals_cumulative) + ")"
+        );
+        const deposit = await contract.deposit({ value: amount });
+        await deposit.wait();
+      }
     }
-  }
 
-  const end_balance = await contract.balanceOf(deployer.address);
-  const expected_balance = start_balance.add(deposits_cumulative).sub(withdrawals_cumulative);
-  expect(end_balance.toString()).toBe(total.toString());
+    const end_balance = await contract.balanceOf(deployer.address);
+    const expected_balance = start_balance.add(deposits_cumulative).sub(withdrawals_cumulative);
+    expect(end_balance.toString()).toBe(total.toString());
+  } catch (error) {
+    decodeAndRethrowError(error);
+  }
 }, 600_000); // TODO: Should be a more thoughtful timeout
