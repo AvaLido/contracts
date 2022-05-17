bff24340e0a96c1a859e15c3c818f563e717e34d
James Nicholson <git@nicholson.io>
2022-05-17 07:44:58 -0400

    Fix linting and build issues

--- src/interfaces/IMPCManager.sol
+++ src/interfaces/IMPCManager.sol
@@ -5,5 +5,10 @@ pragma solidity 0.8.10;
 // TODO: rename serveStake as requestStake
 
 interface IMPCManager {
-    function serveStake(string calldata nodeID, uint256 amount, uint256 startTime, uint256 endTime) external;
+    function serveStake(
+        string calldata nodeID,
+        uint256 amount,
+        uint256 startTime,
+        uint256 endTime
+    ) external;
 }
