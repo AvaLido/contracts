65bd42562687d08c87a4394f4f681b36b0c7e7b7
gldeng <realgldeng@gmail.com>
2022-05-20 16:03:31 +0800

    Change deploy script

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -6,16 +6,18 @@ export VALIDATOR_WHITELIST=["NodeID-P7oB2McjBGgW2NXXWVYjV8JEDFoW9xDE5","NodeID-G
 export ORACLE_WHITELIST=["0x03C1196617387899390d3a98fdBdfD407121BB67","0x6C58f6E7DB68D9F75F2E417aCbB67e7Dd4e413bf","0xa7bB9405eAF98f36e2683Ba7F36828e260BD0018","0xE339767906891bEE026285803DA8d8F2f346842C","0x0309a747a34befD1625b5dcae0B00625FAa30460"]
 
 # These exports used by integration test scripts.
+export MPC_MANAGER=$(task deploy -- MpcManager | grep -i "deployed" | cut -d " " -f 3)
 export ORACLE_MANAGER=$(task deploy -- OracleManager --constructor-args $ROLE_ORACLE_MANAGER $VALIDATOR_WHITELIST $ORACLE_WHITELIST | grep -i "deployed" | cut -d " " -f 3)
 export ORACLE=$(task deploy -- Oracle --constructor-args $ROLE_ORACLE_MANAGER $ORACLE_MANAGER | grep -i "deployed" | cut -d " " -f 3)
 export VALIDATOR_SELECTOR=$(task deploy -- ValidatorSelector --constructor-args $ORACLE | grep -i "deployed" | cut -d " " -f 3)
-export AVALIDO=$(task deploy -- AvaLido --constructor-args "0x2000000000000000000000000000000000000001" "0x2000000000000000000000000000000000000002" $VALIDATOR_SELECTOR "0x3000000000000000000000000000000000000001" | grep -i "deployed" | cut -d " " -f 3)
+export AVALIDO=$(task deploy -- AvaLido --constructor-args "0x2000000000000000000000000000000000000001" "0x2000000000000000000000000000000000000002" $VALIDATOR_SELECTOR $MPC_MANAGER | grep -i "deployed" | cut -d " " -f 3)
 
 # set oracle address from oraclemanager
 # task call -- $ORACLE_MANAGER "setOracleAddress(address)" $ORACLE --from $ROLE_ORACLE_MANAGER --private-key $ROLE_ORACLE_MANAGER_PK
 cast send --rpc-url "http://127.0.0.1:9650/ext/bc/C/rpc" --from $ROLE_ORACLE_MANAGER --private-key $ROLE_ORACLE_MANAGER_PK -- $ORACLE_MANAGER "setOracleAddress(address)" $ORACLE
 
 # Print addresses for easy access
+printf "MpcManager contract deployed to: $MPC_MANAGER\n"
 printf "OracleManager contract deployed to: $ORACLE_MANAGER\n"
 printf "Oracle contract deployed to: $ORACLE\n"
 printf "ValidatorSelector contract deployed to: $VALIDATOR_SELECTOR\n"
