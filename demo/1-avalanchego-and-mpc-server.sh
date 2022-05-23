94d0f1cdd1e3ea6bf46c8533a0fd2933ae1459fe
gldeng <realgldeng@gmail.com>
2022-05-23 17:18:15 +0800

    Make scripts able to run in demo folder

--- demo/1-avalanchego-and-mpc-server.sh
+++ demo/1-avalanchego-and-mpc-server.sh
@@ -5,8 +5,10 @@ mkdir -p logs
 pkill -f avalanchego
 pkill -f mpc-server
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Start avalanchego
-avalanchego --network-id=local --staking-enabled=false --snow-sample-size=1 --snow-quorum-size=1 --genesis ./network/genesis.json --network-id 1337 \
+avalanchego --network-id=local --staking-enabled=false --snow-sample-size=1 --snow-quorum-size=1 --genesis $SCRIPT_DIR/../network/genesis.json --network-id 1337 \
 > logs/node.log 2>&1 &
 
 # Start mpc-server
--- demo/2-deploy.sh
+++ demo/2-deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 export ROLE_DEFAULT_ADMIN="0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC" # Contract Deployer
 export ROLE_DEFAULT_ADMIN_PK="56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027" # Contract deployer PK
 export ROLE_ORACLE_MANAGER=$ROLE_DEFAULT_ADMIN
 export ROLE_ORACLE_MANAGER_PK=$ROLE_DEFAULT_ADMIN_PK
 export VALIDATOR_WHITELIST=["NodeID-P7oB2McjBGgW2NXXWVYjV8JEDFoW9xDE5","NodeID-GWPcbFJZFfZreETSoWjPimr846mXEKCtu","NodeID-NFBbbJ4qCmNaCzeW7sxErhvWqvEQMnYcN"]
 export ORACLE_WHITELIST=["0x03C1196617387899390d3a98fdBdfD407121BB67","0x6C58f6E7DB68D9F75F2E417aCbB67e7Dd4e413bf","0xa7bB9405eAF98f36e2683Ba7F36828e260BD0018","0xE339767906891bEE026285803DA8d8F2f346842C","0x0309a747a34befD1625b5dcae0B00625FAa30460"]
+
+cd $SCRIPT_DIR/..
 
 # These exports used by integration test scripts.
 export MPC_MANAGER=$(task deploy -- MpcManager | grep -i "deployed" | cut -d " " -f 3)
@@ -24,6 +28,7 @@ cast send --rpc-url "http://127.0.0.1:9650/ext/bc/C/rpc" --from $ROLE_DEFAULT_AD
 # task call -- $ORACLE_MANAGER "setOracleAddress(address)" $ORACLE --from $ROLE_ORACLE_MANAGER --private-key $ROLE_ORACLE_MANAGER_PK
 cast send --rpc-url "http://127.0.0.1:9650/ext/bc/C/rpc" --from $ROLE_ORACLE_MANAGER --private-key $ROLE_ORACLE_MANAGER_PK -- $ORACLE_MANAGER "setOracleAddress(address)" $ORACLE
 
+cd $SCRIPT_DIR
 echo -n $MPC_MANAGER > MPC_MANAGER_ADDRESS
 echo -n $AVALIDO > AVALIDO_ADDRESS
 
