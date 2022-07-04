750876f04d282b90c43774b2f0856e0309584bcd
gldeng <realgldeng@gmail.com>
2022-07-04 11:42:23 +0800

    Change staking delay to 5 mins

--- src/mocks/MockAvaLido.sol
+++ src/mocks/MockAvaLido.sol
@@ -29,7 +29,7 @@ contract MockAvaLido {
     }
 
     function initiateStake() external returns (uint256) {
-        uint256 startTime = block.timestamp + 30 seconds;
+        uint256 startTime = block.timestamp + 300 seconds;
         uint256 endTime = startTime + STAKE_PERIOD;
         mpcManager.requestStake{value: amount}(NODE_ID, amount, startTime, endTime);
 
