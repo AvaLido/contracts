93fe7d6cdec5fc2436ac542a8333ec78bc5c792a
James Nicholson <git@nicholson.io>
2022-07-14 12:01:05 +0100

    Replace send with `deposit` method call, fix `cast` args

--- e2e/1-deposit.sh
+++ e2e/1-deposit.sh
@@ -11,7 +11,8 @@ fi
 ETHER=000000000000000000
 VALUE=100000${ETHER}
 
+export RPC_URL="http://127.0.0.1:9650/ext/bc/C/rpc" # Local network
 export ROLE_DEFAULT_ADMIN="0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC" # Contract Deployer
 export ROLE_DEFAULT_ADMIN_PK="56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027" # Contract deployer PK
 
-cast send --rpc-url "http://127.0.0.1:9650/ext/bc/C/rpc" --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value $VALUE -- $AVALIDO_ADDRESS
+cast send --rpc-url $RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value $VALUE -- $AVALIDO_ADDRESS "deposit()"
