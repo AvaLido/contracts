ca0114d76df721b4ca09f53016b04ff0b798e003
James Nicholson <git@nicholson.io>
2022-07-19 17:23:02 +0100

    Fix import

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.10;
 import "openzeppelin-contracts/contracts/security/Pausable.sol";
 import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 import "openzeppelin-contracts/contracts/access/AccessControlEnumerable.sol";
-import "openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 
 import "./interfaces/ITreasury.sol";
 
