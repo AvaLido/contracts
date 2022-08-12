e5053b98683f74be94004e310234810559874278
James Nicholson <git@nicholson.io>
2022-08-12 19:17:25 +0100

    Update `testExploitZeroValueStAVAX` unit test for build changes

--- src/test/AvaLido.t.sol
+++ src/test/AvaLido.t.sol
@@ -1562,23 +1562,27 @@ contract AvaLidoTest is DSTest, Helpers {
     }
 
     function testExploitZeroValueStAVAX() public {
-        // Let a user stake 1 avax and get stAVAX
+        // Increase protocol limit for test (default is low)
+        cheats.prank(DEPLOYER_ADDRESS);
+        lido.setMaxProtocolControlledAVAX(type(uint256).max);
+
+        // Let a user stake 1 AVAX and get stAVAX
         cheats.deal(USER1_ADDRESS, 1 ether);
         cheats.prank(USER1_ADDRESS);
         lido.deposit{value: 1 ether}();
 
         // Attacker now creates 1 stAVAX
-        cheats.deal(USER1_ADDRESS, MAXIMUM_STAKE_AMOUNT);
+        cheats.deal(USER1_ADDRESS, type(uint256).max);
         cheats.prank(USER1_ADDRESS);
         lido.deposit{value: 1 ether}();
 
-        // rate is 1:1 still
+        // Rate is 1:1 still
         assertEq(lido.exchangeRateAVAXToStAVAX(), 1 ether);
 
         // Then attacker forces stAVAX value to zero
         cheats.prank(USER1_ADDRESS);
         for (uint256 index = 0; index < 500; index++) {
-            payable(address(lido)).transfer(100000000 ether);
+            lido.deposit{value: 100000000 ether}();
         }
         assertEq(lido.exchangeRateAVAXToStAVAX(), 1 ether);
     }
